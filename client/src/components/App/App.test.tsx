4e5640af30361fbf2ba1ffba348b3e50449b22f1
Geoff Whitehead <geoff1012@gmail.com>
2021-02-10 22:40:36 +0000

    add some front end tests

--- client/src/components/App/App.test.tsx
+++ client/src/components/App/App.test.tsx
@@ -1,9 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
-import App from "./App";
+import { App } from "./App";
 
-test("renders learn react link", () => {
+test("renders the title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const Header = screen.getByText(/Starship Builder/i);
+  expect(Header).toBeInTheDocument();
+});
+
+test("renders the available starships table", () => {
+  render(<App />);
+  const Header1 = screen.getByText(/Cost/i);
+  const Header2 = screen.getByText(/Crew/i);
+  expect(Header1).toBeInTheDocument();
+  expect(Header2).toBeInTheDocument();
+});
+
+test("renders the starships table", () => {
+  render(<App />);
+  const Header1 = screen.getByText(/Build Progress/i);
+  const Header2 = screen.getByText(/Navigation/i);
+  expect(Header1).toBeInTheDocument();
+  expect(Header2).toBeInTheDocument();
 });
